a4a6c0da3e1391583fec9326c83a66976322036a
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-10-22 08:58:00 -0700

    added drawing

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const grid = document.querySelector("#grid");
 function createBoxes() {
 
-    for (let i = 0; i < 6; i ++) {
+    for (let i = 0; i < 16; i ++) {
         const row = document.createElement("div");
         row.setAttribute("style", "display: flex;");
         grid.appendChild(row);
-        for (let l = 0; l < 6; l ++) {
+        for (let l = 0; l < 16; l ++) {
             const box = document.createElement("div");
-            box.setAttribute("style", "outline: solid 1px gray; width: 50px; height: 50px;");
+            box.setAttribute("style", "outline: solid 1px gray; width: 30px; height: 30px;");
             row.appendChild(box);
+            box.addEventListener("mouseover", () => {
+                box.style["background-color"] = "black";
+            })
         }
         
     }
