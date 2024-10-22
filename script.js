59b6e915946edf49d6d97d30273f601ae84107a8
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-10-22 12:12:34 -0700

    added clear button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const grid = document.querySelector("#grid");
 function createBoxes() {
-
+    const clear = document.querySelector("#clear");
+    clear.addEventListener("click", () => {
+        clearAll();
+    })
     for (let i = 0; i < 16; i ++) {
         const row = document.createElement("div");
         row.setAttribute("style", "display: flex;");
         grid.appendChild(row);
         for (let l = 0; l < 16; l ++) {
             const box = document.createElement("div");
-            box.setAttribute("style", "outline: solid 1px gray; width: 30px; height: 30px;");
+            box.setAttribute("style", "width: 30px; height: 30px; background-color: white");
             box.classList.add("gridBox");
             row.appendChild(box);
             box.addEventListener("mouseover", () => {
