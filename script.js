41b7bfb03cc73e50018647d27854799f41afa5dc
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2024-10-22 08:42:20 -0700

    fixed grid and centered objects

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const grid = document.querySelector("#grid");
-
 function createBoxes() {
 
-    for (let i = 0; i < 4; i ++) {
+    for (let i = 0; i < 6; i ++) {
         const row = document.createElement("div");
         row.setAttribute("style", "display: flex;");
         grid.appendChild(row);
-        for (let l = 0; l < 4; l ++) {
+        for (let l = 0; l < 6; l ++) {
             const box = document.createElement("div");
-            box.setAttribute("style", "outline: solid 2px black; width: 50px; height: 50px;");
+            box.setAttribute("style", "outline: solid 1px gray; width: 50px; height: 50px;");
             row.appendChild(box);
         }
         
